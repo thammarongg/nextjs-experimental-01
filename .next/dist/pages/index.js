39ff5121082f48c44cc41cf749b8ef562e5df3fc
MISNB-01\MIS <thammarongg@meyer-mil.com>
2017-08-25 00:27:58 +0700

    add next-routes to test beautiful route

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "C:\\Projects\\GitHub\\nextjs-experimental-01\\pages\\index.js?entry";
+
+var Index = function Index() {
+  return _react2.default.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 1
+    }
+  }, "Welcome");
+};
+
+exports.default = Index;
